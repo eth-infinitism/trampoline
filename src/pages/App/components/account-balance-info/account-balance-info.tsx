3ba21412218f94740f86dc44edbf0da6424b84ca
Garvit Khatri <garvitdelhi@gmail.com>
2023-02-03 15:32:23 +0800

    Add wagmi and wallet deployed status

--- src/pages/App/components/account-balance-info/account-balance-info.tsx
+++ src/pages/App/components/account-balance-info/account-balance-info.tsx
@@ -1,11 +1,22 @@
-import { Stack, Typography, Chip } from '@mui/material';
-import React from 'react';
+import { Stack, Typography, Chip, Tooltip } from '@mui/material';
+import React, { useEffect, useState } from 'react';
 import { getActiveNetwork } from '../../../Background/redux-slices/selectors/networkSelectors';
 import { useBackgroundSelector } from '../../hooks';
 import CancelIcon from '@mui/icons-material/Cancel';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import { getProvider } from '@wagmi/core';
 
-const AccountBalanceInfo = () => {
+const AccountBalanceInfo = ({ address }: { address: string }) => {
   const activeNetwork = useBackgroundSelector(getActiveNetwork);
+  const [walletDeployed, setWalletDeployed] = useState<boolean>(false);
+
+  const provider = getProvider();
+
+  useEffect(() => {
+    provider.getCode(address).then((code) => {
+      if (code !== '0x') setWalletDeployed(true);
+    });
+  }, [provider, address, setWalletDeployed]);
 
   return (
     <Stack spacing={1} justifyContent="center" alignItems="center">
@@ -17,13 +28,21 @@ const AccountBalanceInfo = () => {
         />
       )}
       <Typography variant="h3">0 ETH</Typography>
-      <Chip
-        variant="outlined"
-        color="error"
-        size="small"
-        icon={<CancelIcon />}
-        label="Not deployed"
-      />
+      <Tooltip
+        title={
+          walletDeployed
+            ? `Wallet has been deployed on ${activeNetwork.name} chain`
+            : `Wallet is not deployed on ${activeNetwork.name} chain, it will be deployed upon the first transaction`
+        }
+      >
+        <Chip
+          variant="outlined"
+          color={walletDeployed ? 'success' : 'error'}
+          size="small"
+          icon={walletDeployed ? <CheckCircleIcon /> : <CancelIcon />}
+          label={walletDeployed ? 'Deployed' : 'Not deployed'}
+        />
+      </Tooltip>
     </Stack>
   );
 };
