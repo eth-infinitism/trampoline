01fc65e4e6aae5713c7d1af7dfc3c2d8e7c2b96e
plusminushalf <plusminushalf@gmail.com>
2023-02-03 15:32:23 +0800

    Add wagmi and wallet deployed status

--- src/pages/App/pages/home/home.tsx
+++ src/pages/App/pages/home/home.tsx
@@ -32,7 +32,7 @@ const Home = () => {
             alignItems="center"
             sx={{ m: 2 }}
           >
-            <AccountBalanceInfo />
+            <AccountBalanceInfo address={activeAccount} />
           </Box>
           <Box
             component="div"
