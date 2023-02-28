f658b6383b3bc0fc610c1233d653c43ce2ae0589
Garvit Khatri <garvitdelhi@gmail.com>
2023-02-28 07:07:04 -0700

    Add deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -4,23 +4,20 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
-const hre = require("hardhat");
+import { ethers } from 'hardhat';
+import Config from '../src/exconfig.json';
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-  const lockedAmount = hre.ethers.utils.parseEther("1");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+  const TwoOwnerAccountFactory = await ethers.getContractFactory(
+    'TwoOwnerAccountFactory'
   );
+  const twoOwnerAccount = await TwoOwnerAccountFactory.deploy(
+    Config.network.entryPointAddress
+  );
+
+  await twoOwnerAccount.deployed();
+
+  console.log(`TwoOwnerAccountFactory deployed to ${twoOwnerAccount.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
