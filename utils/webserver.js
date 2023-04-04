591cd811d7744873732b56eaa8dc4d52da35a8ee
Garvit Khatri <garvitdelhi@gmail.com>
2023-04-04 13:38:27 +0530

    Fix auto hot reload

--- utils/webserver.js
+++ utils/webserver.js
@@ -3,6 +3,9 @@ process.env.BABEL_ENV = 'development';
 process.env.NODE_ENV = 'development';
 process.env.ASSET_PATH = '/';
 
+const SSEStream = require('ssestream').default;
+const debounce = require('lodash').debounce;
+
 var WebpackDevServer = require('webpack-dev-server'),
   webpack = require('webpack'),
   config = require('../webpack.config'),
@@ -21,6 +24,19 @@ for (var entryName in config.entry) {
   }
 }
 
+if (options.enableBackgroundAutoReload) {
+  config.entry['background'] = [
+    path.resolve(
+      __dirname,
+      `autoReloadClients/backgroundClient.js?port=${env.PORT}`
+    ),
+  ].concat(config.entry['background']);
+}
+
+config.plugins = [new webpack.HotModuleReplacementPlugin()].concat(
+  config.plugins || []
+);
+
 delete config.chromeExtensionBoilerplate;
 
 var compiler = webpack(config);
@@ -28,12 +44,9 @@ var compiler = webpack(config);
 var server = new WebpackDevServer(
   {
     https: false,
-    hot: true,
-    liveReload: false,
-    client: {
-      webSocketTransport: 'ws',
-    },
-    webSocketServer: 'ws',
+    hot: false,
+    client: false,
+    compress: false,
     host: 'localhost',
     port: env.PORT,
     static: {
@@ -42,6 +55,71 @@ var server = new WebpackDevServer(
     devMiddleware: {
       publicPath: `http://localhost:${env.PORT}/`,
       writeToDisk: true,
+    },
+    setupMiddlewares: (middlewares, devServer) => {
+      if (!options.enableBackgroundAutoReload) {
+        return middlewares;
+      }
+      if (!devServer) {
+        throw new Error('webpack-dev-server is not defined');
+      }
+
+      middlewares.push({
+        path: '/__server_sent_events__', // you can find this path requested by backgroundClient.js.
+        middleware: (req, res) => {
+          const sseStream = new SSEStream(req);
+          sseStream.pipe(res);
+
+          sseStream.write('message from webserver.');
+
+          let closed = false;
+
+          const compileDoneHook = debounce((stats) => {
+            const { modules } = stats.toJson({ all: false, modules: true });
+            const updatedJsModules = modules.filter(
+              (module) =>
+                module.type === 'module' &&
+                module.moduleType === 'javascript/auto'
+            );
+            const isBackgroundUpdated = updatedJsModules.some((module) =>
+              module.nameForCondition.startsWith(
+                path.resolve(__dirname, '../src/pages/Background')
+              )
+            );
+            const shouldBackgroundReload =
+              !stats.hasErrors() &&
+              isBackgroundUpdated &&
+              options.enableBackgroundAutoReload;
+
+            if (shouldBackgroundReload) {
+              sseStream.writeMessage(
+                {
+                  event: 'background-updated',
+                  data: {}, // "data" key should be reserved though it is empty.
+                },
+                'utf-8'
+              );
+            }
+          }, 1000);
+
+          const plugin = (stats) => {
+            if (!closed) {
+              compileDoneHook(stats);
+            }
+          };
+
+          // a mini webpack plugin just born!
+          // this plugin will be triggered after each compilation done.
+          compiler.hooks.done.tap('extension-auto-reload-plugin', plugin);
+
+          res.on('close', () => {
+            closed = true;
+            sseStream.unpipe(res);
+          });
+        },
+      });
+
+      return middlewares;
     },
     headers: {
       'Access-Control-Allow-Origin': '*',
