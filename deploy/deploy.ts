a73c384eae6d860ff5b00caec4c5e2b3624f7fec
plusminushalf <plusminushalf@gmail.com>
2023-05-08 11:35:52 +0530

    Add TwoOwnerAccountFactory deploy script

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -1,12 +1,14 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/types';
+import config from '../src/exconfig';
+import fs from 'fs';
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const accounts = await hre.getUnnamedAccounts();
-  await hre.deployments.deploy('Greeter', {
+  await hre.deployments.deploy('TwoOwnerAccountFactory', {
     from: accounts[0],
     deterministicDeployment: true,
-    args: ['Test'],
+    args: [config.network.entryPointAddress],
     log: true,
   });
 };
