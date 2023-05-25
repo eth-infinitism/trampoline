1297cd447b8d09c70a38123c3d154fab3199ee9b
plusminushalf <plusminushalf@gmail.com>
2023-05-26 00:05:02 +0530

    Make default wallet extend SimpleWallet

--- src/pages/Account/account-api/account-api.ts
+++ src/pages/Account/account-api/account-api.ts
@@ -1,12 +1,9 @@
 import { BigNumber, BigNumberish, ethers, Wallet } from 'ethers';
 import {
   SimpleAccount,
-  SimpleAccount__factory,
   SimpleAccountFactory,
-  SimpleAccountFactory__factory,
   UserOperationStruct,
 } from '@account-abstraction/contracts';
-import { arrayify, hexConcat } from 'ethers/lib/utils';
 
 import { AccountApiParamsType, AccountApiType } from './types';
 import { MessageSigningRequest } from '../../Background/redux-slices/signing';
@@ -24,75 +21,32 @@ const FACTORY_ADDRESS =
  * - nonce method is "nonce()"
  * - execute method is "execFromEntryPoint()"
  */
-class SimpleAccountTrampolineAPI extends AccountApiType {
-  factoryAddress?: string;
-  owner: Wallet;
-  index: number;
-
+class SimpleAccountTrampolineAPI
+  extends SimpleAccountAPI
+  implements AccountApiType
+{
   /**
    * our account contract.
    * should support the "execFromEntryPoint" and "nonce" methods
    */
-  simpleAccountApiInstance: SimpleAccountAPI;
-
   factory?: SimpleAccountFactory;
 
   constructor(params: AccountApiParamsType<{}, { privateKey: string }>) {
-    super(params);
-    this.factoryAddress = FACTORY_ADDRESS;
-
-    this.owner = params.deserializeState?.privateKey
-      ? new ethers.Wallet(params.deserializeState?.privateKey)
-      : ethers.Wallet.createRandom();
-    this.index = 0;
-
-    this.simpleAccountApiInstance = new SimpleAccountAPI({
-      provider: this.provider,
-      owner: this.owner,
-      factoryAddress: this.factoryAddress,
-      entryPointAddress: params.entryPointAddress,
-      index: this.index,
+    super({
+      ...params,
+      index: 0,
+      owner: params.deserializeState?.privateKey
+        ? new ethers.Wallet(params.deserializeState?.privateKey)
+        : ethers.Wallet.createRandom(),
+      factoryAddress: FACTORY_ADDRESS,
     });
   }
 
   serialize = async (): Promise<{ privateKey: string }> => {
     return {
-      privateKey: this.owner.privateKey,
+      privateKey: (this.owner as Wallet).privateKey,
     };
   };
-
-  async _getAccountContract(): Promise<SimpleAccount> {
-    return this.simpleAccountApiInstance._getAccountContract();
-  }
-
-  /**
-   * return the value to put into the "initCode" field, if the account is not yet deployed.
-   * this value holds the "factory" address, followed by this account's information
-   */
-  async getAccountInitCode(): Promise<string> {
-    return this.simpleAccountApiInstance.getAccountInitCode();
-  }
-
-  async getNonce(): Promise<BigNumber> {
-    return this.simpleAccountApiInstance.getNonce();
-  }
-
-  /**
-   * encode a method call from entryPoint to our contract
-   * @param target
-   * @param value
-   * @param data
-   */
-  async encodeExecute(
-    target: string,
-    value: BigNumberish,
-    data: string
-  ): Promise<string> {
-    return this.simpleAccountApiInstance.encodeExecute(target, value, data);
-  }
-  async signUserOpHash(userOpHash: string): Promise<string> {
-    return this.simpleAccountApiInstance.signUserOpHash(userOpHash);
-  }
 
   signMessage = async (
     context: any,
@@ -106,7 +60,7 @@ class SimpleAccountTrampolineAPI extends AccountApiType {
     preTransactionConfirmationContext?: any
   ): Promise<UserOperationStruct> {
     return {
-      ...(await this.simpleAccountApiInstance.createUnsignedUserOp(info)),
+      ...(await this.createUnsignedUserOp(info)),
       paymasterAndData: preTransactionConfirmationContext?.paymasterAndData
         ? preTransactionConfirmationContext?.paymasterAndData
         : '0x',
