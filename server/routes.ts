db37ef5b802bb258c95f3c22e13d441253c8cea9
plusminushalf <garvitdelhi@gmail.com>
2023-07-12 14:46:18 +0530

    Add dummy signatures & Alchemy implementation

--- server/routes.ts
+++ server/routes.ts
@@ -1,17 +1,95 @@
 import express, { Express } from 'express';
 import { ethers } from 'ethers';
+import { UserOperationStruct } from '@account-abstraction/contracts';
 
 const apirouter = express.Router();
 
 const paymasterRouter = express.Router();
 
+const getStackupPaymasterAndData = async (
+  provider: ethers.providers.JsonRpcProvider,
+  {
+    userOp,
+    entryPoint,
+  }: {
+    userOp: UserOperationStruct;
+    entryPoint: string;
+  }
+): Promise<{
+  callGasLimit: string;
+  paymasterAndData: string;
+  preVerificationGas: string;
+  verificationGasLimit: string;
+}> => {
+  const paymasterRPC = new ethers.providers.JsonRpcProvider(
+    process.env.PAYMASTER_SERVICE_URL,
+    {
+      name: 'Paymaster',
+      chainId: (await provider.getNetwork()).chainId,
+    }
+  );
+
+  const response: {
+    callGasLimit: string;
+    paymasterAndData: string;
+    preVerificationGas: string;
+    verificationGasLimit: string;
+  } = await paymasterRPC.send('pm_sponsorUserOperation', [
+    userOp,
+    entryPoint,
+    {
+      type: 'payg',
+    },
+  ]);
+
+  return response;
+};
+
+const getAlchecmyPaymasterAndData = async (
+  provider: ethers.providers.JsonRpcProvider,
+  {
+    userOp,
+    entryPoint,
+  }: {
+    userOp: UserOperationStruct;
+    entryPoint: string;
+  }
+): Promise<{
+  callGasLimit: string;
+  paymasterAndData: string;
+  preVerificationGas: string;
+  verificationGasLimit: string;
+}> => {
+  const paymasterRPC = new ethers.providers.JsonRpcProvider(
+    process.env.PAYMASTER_SERVICE_URL,
+    {
+      name: 'Paymaster',
+      chainId: (await provider.getNetwork()).chainId,
+    }
+  );
+
+  const response: {
+    callGasLimit: string;
+    paymasterAndData: string;
+    preVerificationGas: string;
+    verificationGasLimit: string;
+  } = await paymasterRPC.send('alchemy_requestGasAndPaymasterAndData', [
+    {
+      policyId: process.env.POLICY_ID,
+      dummySignature: userOp.signature,
+      entryPoint,
+      userOperation: userOp,
+    },
+  ]);
+
+  return response;
+};
+
 paymasterRouter.route('/').post(async (req, res) => {
   const {
     method,
     params: [userOp, entryPoint],
-  } = req.body;
-
-  console.log(method, userOp, entryPoint);
+  }: { method: string; params: [UserOperationStruct, string] } = req.body;
 
   if (!method || !userOp || !entryPoint) {
     res.status(400).send('Bad Request');
@@ -24,37 +102,41 @@ paymasterRouter.route('/').post(async (req, res) => {
 
   switch (req.body.method) {
     case 'local_getPaymasterAndData':
-      const paymasterRPC = new ethers.providers.JsonRpcProvider(
-        process.env.PAYMASTER_SERVICE_URL,
-        {
-          name: 'Paymaster',
-          chainId: (await provider.getNetwork()).chainId,
-        }
-      );
+      /**
+       * Alchemy's implementation
+       */
+      //   const {
+      //     callGasLimit,
+      //     paymasterAndData,
+      //     preVerificationGas,
+      //     verificationGasLimit,
+      //   } = await getAlchecmyPaymasterAndData(provider, {
+      //     userOp,
+      //     entryPoint,
+      //   });
 
+      /**
+       * Stackup's implementation
+       */
       const {
         callGasLimit,
         paymasterAndData,
         preVerificationGas,
         verificationGasLimit,
-      }: {
-        callGasLimit: string;
-        paymasterAndData: string;
-        preVerificationGas: string;
-        verificationGasLimit: string;
-      } = await paymasterRPC.send('pm_sponsorUserOperation', [
+      } = await getStackupPaymasterAndData(provider, {
         userOp,
         entryPoint,
-        {
-          type: 'payg',
-        },
-      ]);
+      });
 
       res.send({
-        callGasLimit,
-        paymasterAndData,
-        preVerificationGas,
-        verificationGasLimit,
+        id: req.body.id,
+        jsonrpc: '2.0',
+        result: {
+          callGasLimit,
+          paymasterAndData,
+          preVerificationGas,
+          verificationGasLimit,
+        },
       });
       break;
     default:
