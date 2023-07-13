35227816f5a3ab3fb430967dc79de86638356183
Andrew Morris <voltrevo@gmail.com>
2023-07-13 15:23:44 +1000

    Add docs

--- src/helpers/ensureError.ts
+++ src/helpers/ensureError.ts
@@ -1,3 +1,9 @@
+/**
+ * By convention, exceptions should always be `Error`s, but it's only a
+ * convention, so we don't actually get any type information on exceptions.
+ * This functions wraps an exception and just returns it unchanged if it's
+ * an `Error`, otherwise it wraps it in an `Error`.
+ */
 export default function ensureError<E>(e: E): E extends Error ? E : Error {
   if (e instanceof Error) {
     return e as E extends Error ? E : Error;
--- src/helpers/getEthereumGlobal.ts
+++ src/helpers/getEthereumGlobal.ts
@@ -1,5 +1,16 @@
 import { BigNumberish } from "ethers";
 
+/**
+ * Gets the ethereum global in a convenient way.
+ * 
+ * It does two important things:
+ * - Throws an exception if window.ethereum is missing so that we can return a
+ *   non-optional `ethereum` value.
+ * - Adds extra type information. Our type information for window.ethereum
+ *   currently comes from node_modules/@wagmi/connectors/dist/types-86dbb446.d.ts,
+ *   which is very incomplete and doesn't seem entirely intentional. This might
+ *   be resolved by updating wagmi.
+ */
 export default function getEthereumGlobal() {
   const windowEthereum = window.ethereum;
 
