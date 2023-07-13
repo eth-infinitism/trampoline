ba50309995c4d965d5dbae4e1009d22c9b89b3d5
Garvit Khatri <garvitdelhi@gmail.com>
2023-07-13 15:23:44 +1000

    Add docs

--- src/helpers/omit.ts
+++ src/helpers/omit.ts
@@ -1,4 +1,14 @@
-export default function omit<Obj extends Record<string | symbol, unknown>, K extends keyof Obj>(obj: Obj, key: K): Omit<Obj, K> {
+/**
+ * Removes a key from an object without modifying it.
+ * 
+ * This can be an improvement over `{ ...obj, key: undefined }` since it
+ * actually removes the key and doesn't introduce `undefined` into the type
+ * information.
+ */
+export default function omit<
+  Obj extends Record<string | symbol, unknown>,
+  K extends keyof Obj
+>(obj: Obj, key: K): Omit<Obj, K> {
   const newObj = { ...obj };
   delete newObj[key];
 
